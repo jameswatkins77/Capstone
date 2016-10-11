31260213da20b7fa080911abdd6d8651aaa9120a
Jim Watkins <james.watkins77@yahoo.com>
2016-10-10 20:40:44 -0600

    changed logo

--- www/templates/splash/splash.js
+++ www/templates/splash/splash.js
@@ -1,6 +1,7 @@
 'Use Strict';
-angular.module('capstone').controller('SplashCtrl',['$scope', '$rootScope', '$state', '$log', '$location', '$http', '$ionicPopup', '$firebaseObject', 'FURL',
-function($scope, $rootScope, $state, $log, $location, $http, $ionicPopup, $firebaseObject, FURL) {
+angular.module('capstone').controller('SplashCtrl',['$scope', '$timeout', '$rootScope', '$state', '$log', '$location', '$http', '$ionicPopup', '$firebaseObject', 'FURL',
+function($scope, $timeout, $rootScope, $state, $log, $location, $http, $ionicPopup, $firebaseObject, FURL) {
   $scope.data = {};
   $scope.data.title = "Chore Score";
+  $timeout( function(){ $state.go("login"); }, 2000);
 }]);
