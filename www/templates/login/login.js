31fe9822ec8e3d7de952628b0d1ce0e6737fdd30
Jim Watkins <james.watkins77@yahoo.com>
2016-10-18 19:56:36 -0600

    added error alert for incorrect login

--- www/templates/login/login.js
+++ www/templates/login/login.js
@@ -1,26 +1,35 @@
 'Use Strict';
 angular.module('capstone').controller('loginCtrl', function($scope, $rootScope, $firebaseArray, $firebaseAuth, $state, $log, $location, $http, $ionicPopup, $firebaseObject, user, FURL) {
   $scope.data = {};
+  $scope.data.error;
 
   $scope.data.login = function(user){
     firebase.auth().signInWithEmailAndPassword(user.email, user.password).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
+      return $scope.data.error = error.message;
     }).then(function(response){
-      var uid = firebase.auth().currentUser.uid;
-      var ref = firebase.database().ref().child('users');
-      ref.on("value", function(snapshot) {
-    		let data = snapshot.val();
-        for (var id in data) {
-          if (uid === data[id].id) {
-            if (data[id].type === "parent") {
-              $state.go("parentHome")
-            } else {
-              $state.go("childHome")
+      $scope.data.error;
+      if ($scope.data.error) {
+        alert("Username and/or password is incorrect.  Please try again.")
+      } else {
+        var uid = firebase.auth().currentUser.uid;
+        var ref = firebase.database().ref().child('users');
+        ref.on("value", function(snapshot) {
+          let data = snapshot.val();
+          for (var id in data) {
+            if (uid === data[id].id) {
+              if (data[id].type === "parent") {
+                console.log("parent logged in");
+                $state.go("parentHome")
+              } else {
+                console.log("child logged in");
+                $state.go("childHome")
+              }
             }
           }
-        }
-    	})
+        })
+      }
     });
   }
 });
